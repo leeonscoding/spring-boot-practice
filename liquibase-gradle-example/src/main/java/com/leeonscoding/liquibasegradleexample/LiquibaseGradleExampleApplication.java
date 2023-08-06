9ee781919bbe78231b630e1232a2b092d6f104bc
leeonscoding <leeonscoding@gmail.com>
2023-08-07 00:39:49 +0600

    initial for liquibase

---
+++ liquibase-gradle-example/src/main/java/com/leeonscoding/liquibasegradleexample/LiquibaseGradleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.leeonscoding.liquibasegradleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseGradleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseGradleExampleApplication.class, args);
+	}
+
+}
---
+++ liquibase-gradle-example/src/test/java/com/leeonscoding/liquibasegradleexample/LiquibaseGradleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeonscoding.liquibasegradleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseGradleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
